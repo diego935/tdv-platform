181af70a412885bcce3264da418c3a65f5e6bddd
Hector del Caño <hectoruqui@gmail.com>
2026-05-05 20:26:07 +0200

    testing de texturas 9

--- src/entities/player.py
+++ src/entities/player.py
@@ -38,7 +38,7 @@ class Jugador(arcade.Sprite):
             "right": assets.get_texture("assets/Jugador/Soldado hacia derecha.png"),
         }
         self.texture = self.texturas[self.direccion]
-        self.scale = 0.025
+        self.scale = 0.035
 
 
     def draw_inventory(self, mouse_pos=None): 
