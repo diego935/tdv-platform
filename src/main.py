4baba2d37b97b18b4ca1c41d113f350d6a47b9fc
cristianbalasoiu <cristian.balasoiu@uah.edu.es>
2026-05-06 21:39:31 +0200

    pantalla completa implementada

--- src/main.py
+++ src/main.py
@@ -2,8 +2,22 @@ import arcade
 from config import ANCHO_VENTANA, ALTO_VENTANA, TITULO_VENTANA
 from vista.menu_principal import MenuPrincipal
 
+class PantallaCompleta(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title, fullscreen=True)
+
+    def on_key_press(self, key, modifiers):
+        # PRESIONAR ESCAPE PARA SALIR DE PANTALLA COMPLETA y F11 PARA VOVERLA A PONER
+        if key == arcade.key.ESCAPE or key == arcade.key.F11:
+            self.set_fullscreen(not self.fullscreen)
+            
+            if not self.fullscreen:
+                self.center_window()
+                
+
 def main():
-    window = arcade.Window(ANCHO_VENTANA, ALTO_VENTANA, TITULO_VENTANA)
+    
+    window = PantallaCompleta(ANCHO_VENTANA, ALTO_VENTANA, TITULO_VENTANA)
     vista_menu = MenuPrincipal()
     window.show_view(vista_menu)
     arcade.run()
