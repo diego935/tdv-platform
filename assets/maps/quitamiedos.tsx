5f37cc00bb194cdb4a60bd422339117c27b14d51
ID4N1 <daniel.martingarcia@edu.uah.es>
2026-05-07 16:53:54 +0200

    Mapa nuevo y nuevos sprites

---
+++ assets/maps/quitamiedos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="quitamiedos" tilewidth="32" tileheight="32" tilecount="4187" columns="53">
+ <image source="../tilesets/quitamiedos.png" width="1696" height="2528"/>
+</tileset>
