cc92598274ccd38dcdc8319e5ebf8019f763acf6
ID4N1 <daniel.martingarcia@edu.uah.es>
2026-05-14 16:29:35 +0200

    Mapa en cruz

---
+++ assets/tilesets/muroCruz.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="muroCruz" tilewidth="32" tileheight="32" tilecount="306" columns="17">
+ <image source="muroCruz.png" width="563" height="579"/>
+</tileset>
---
+++ assets/tilesets/muroEsquinas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="muroEsquinas" tilewidth="32" tileheight="32" tilecount="306" columns="18">
+ <image source="muroEsquinas.png" width="579" height="561"/>
+</tileset>
