66e2f405ba213b2610cd25738c3c88c09902bb27
ID4N1 <daniel.martingarcia@edu.uah.es>
2026-05-14 16:38:55 +0200

    Quitado fondo trampas, cambiado a /assets/items y cambiado el path relativo en codigo

--- src/items/colections.py
+++ src/items/colections.py
@@ -121,9 +121,9 @@ class SpikeTrap(arcade.Sprite):
     def __init__(self, x, y, damage_veneno = 20, tiempo_veneno= 3, tiempo_slow = 5):
         
         if (damage_veneno >0 and tiempo_veneno > 0): 
-            texture = AssetManager().get_texture("assets/tilesets/trampa.png")
+            texture = AssetManager().get_texture("assets/items/trampa.png")
         else: 
-            texture = AssetManager().get_texture("assets/tilesets/trampaVenenosa.png")
+            texture = AssetManager().get_texture("assets/items/trampaVenenosa.png")
         
         super().__init__(texture, scale=0.045)
         
